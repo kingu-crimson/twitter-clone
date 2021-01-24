d17b49358d0800e01b1bec323113269f31e157de
Nourasqar <noorashqar123@gmail.com>
2021-01-24 21:41:25 +0200

    frontend setup

---
+++ client/src/Components/Pages/Bookmarks.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Bookmarks.css'
+import { Link } from "react-router-dom";
+import Header from "../SharedComponents/Header"
+
+class Bookmarks extends React.Component {
+    componentDidMount() {
+
+    }
+    render() {
+
+        return (
+            <>
+            </>
+        )
+    }
+}
+
+
+
+export default Bookmarks
