f72d5b9dfeab533e1a64bc446d269fd80457bc56
Moumenh <mohanbali94@gmail.com>
2021-01-24 19:04:40 +0200

    inner serializing

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -4,11 +4,11 @@ from users.models import UserAccount
 
 # Create your models here.
 class Comment(models.Model):
-    user_id = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(UserAccount, on_delete=models.CASCADE, related_name='user_comments')
     tweet_id = models.ForeignKey(Tweet, on_delete=models.CASCADE, related_name='comments')
     content = models.CharField(max_length=5000)
     image = models.CharField(max_length=1000, default='', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.user_id.name
+        return "%s %s %s" % (self.user_id.name, self.tweet_id.content, self.content)
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,6 +2,8 @@ from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 
 from tweets.serializers import TweetSerializer
+from comments.serializers import CommentSerializer
+from likes.serializers import LikeSerializer
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -13,6 +15,8 @@ class UserCreateSerializer(UserCreateSerializer):
 
 class UserSerializered(serializers.ModelSerializer):
     tweets = TweetSerializer(many=True, read_only=True)
+    user_comments = CommentSerializer(many=True, read_only=True)
+    user_likes = LikeSerializer(many=True, read_only=True)
     class Meta:
         model = User
-        fields = ('id' , 'email', 'name', 'image', 'tweets')
+        fields = ('id' , 'email', 'name', 'image', 'tweets', 'user_comments','user_likes')
