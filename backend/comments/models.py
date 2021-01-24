e66bb105508d8e779cb1c6d45ad2c21a53d11c2a
Moumenh <mohanbali94@gmail.com>
2021-01-24 18:11:50 +0200

    not required comment images

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -7,7 +7,7 @@ class Comment(models.Model):
     user_id = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
     tweet_id = models.ForeignKey(Tweet, on_delete=models.CASCADE, related_name='comments')
     content = models.CharField(max_length=5000)
-    image = models.CharField(max_length=1000)
+    image = models.CharField(max_length=1000, default='', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
