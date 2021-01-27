837b73ddf05b92a91dcf7b41a9a84d817056e1d4
Nourasqar <noorashqar123@gmail.com>
2021-01-27 22:35:17 +0200

    login auth

---
+++ client/src/Components/Pages/Signup.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Signup'
+import { Link } from "react-router-dom";
+import Header from "../SharedComponents/Header"
+class Signup extends React.Component {
+    componentDidMount() {
+
+    }
+    render() {
+
+        return (
+            <>
+            </>
+        )
+    }
+}
+
+
+
+export default Signup
