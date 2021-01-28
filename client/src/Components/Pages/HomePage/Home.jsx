8713d88c8b7f0de0068740a8530c9109a53eef41
Moumenh <mohanbali94@gmail.com>
2021-01-28 03:10:33 +0200

    small bugs

--- client/src/Components/Pages/HomePage/Home.jsx
+++ client/src/Components/Pages/HomePage/Home.jsx
@@ -3,19 +3,13 @@ import { Link } from "react-router-dom";
 import Tweet from './Tweet'
 
 import './Home.css'
-class Home extends React.Component {
-    componentDidMount() {
-
-    }
-    render() {
-
-        return (
-            <div className='home'>
-                {/* <div className='home__img'></div> */}
-                <Tweet />
-            </div>
-        )
-    }
+const Home = () => {
+    return (
+        <div className='home'>
+            {/* <div className='home__img'></div> */}
+            <Tweet />
+        </div>
+    )
 }
 
 
