6c6b3bfc0811f9432146639723439eec86713804
Moumenh <mohanbali94@gmail.com>
2021-01-30 00:49:36 +0200

    follow functionality

--- backend/followers/models.py
+++ backend/followers/models.py
@@ -7,6 +7,9 @@ class Follower(models.Model):
     user_from = models.ForeignKey(UserAccount, on_delete=models.CASCADE, related_name='userTo')
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        unique_together = ('user_to','user_from')
+
     def __str__(self):
         return "%s %s" % (self.user_from.name, self.user_to.name)
 
