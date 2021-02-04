f234c565ed1a0276773c6fdcadc484eeb0419d8f
Moumenh <mohanbali94@gmail.com>
2021-02-05 01:53:38 +0200

    search tweets

--- backend/tweets/urls.py
+++ backend/tweets/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from tweets.views import TweetList, details, bookmakrs, following
+from tweets.views import TweetList, details, bookmakrs, following, search
 
 urlpatterns = [
     path('', TweetList.as_view()),
     path('details', details),
     path('bookmarks', bookmakrs),
-    path('following', following)
+    path('following', following),
+    path('search', search)
 ]
--- client/src/Components/Pages/Explore/Explore.jsx
+++ client/src/Components/Pages/Explore/Explore.jsx
@@ -1,17 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import Tweets from '../Home/Tweets'
+import SearchIcon from '@material-ui/icons/Search';
 
 import './Explore.css'
 const Explore = () => {
 
     const [tweets, setTweets] = useState(null)
-    // console.log('changed', tweets)
+    const [input, setInput] = useState('')
 
     useEffect(() => {
         getTweets()
     }, [])
 
 
+    const searchTweets = () => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ content: input })
+        }
+        fetch('http://127.0.0.1:8000/tweet/search', requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                console.log('succes', data)
+                setTweets(data)
+            })
+    }
 
     const getTweets = () => {
         const requestOptions = {
@@ -28,6 +42,10 @@ const Explore = () => {
 
     return (
         <div className='explore'>
+            <form className='explore__search'>
+                <input className='search__input' type='text' placeholder='Search ..' value={input} onChange={(e) => setInput(e.target.value)} />
+                <SearchIcon className='search__icon' onClick={searchTweets} />
+            </form>
             <Tweets tweets={tweets} />
         </div>
     )
